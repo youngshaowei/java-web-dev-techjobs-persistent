a41f790e7ef923c55bca14f179371f34f5ca170b
Krystal Farwig <youngshaowei@gmail.com>
2020-01-30 19:24:47 -0600

    Finished Part 1 and Part 2 up to Controllers. Added Repositories.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message="Location is required.")
+    @Size(min=3, max=100, message="Location must be between 3 and 100 characters.")
+    private String location;
 
+    public String getLocation() {
+        return location;
+    }
+
+    public Employer() {
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,15 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @Size(max=255, message="Description too long!")
+    private String description;
+
+    public Skill() {
+    }
 
 }
